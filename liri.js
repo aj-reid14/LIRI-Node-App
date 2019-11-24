e8fa75d4274733a57c69f008e2710405d5c66939
ajreid <axr490@miami.edu>
2019-11-23 19:53:39 -0500

    added functionality for 'concert-this' action

--- liri.js
+++ liri.js
@@ -1,3 +1,63 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+const axios = require("axios");
+const Spotify = require("node-spotify-api");
+const moment = require("moment");
+moment().format();
+
+let keys = require("./keys.js");
+let spotify = new Spotify(keys.spotify);
+
+
+let action = process.argv[2];
+let search = process.argv.slice(3);
+
+console.log(action);
+console.log(search);
+
+switch (action) {
+    case "concert-this":
+        ConcertThis(search.join("+"));
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        break;
+    case "do-what-it-says":
+        break;
+}
+
+function ConcertThis(q) {
+
+    axios.get(`https://rest.bandsintown.com/artists/${q}/events?app_id=codingbootcamp`).then(function(response) {
+
+    let event = response.data[0];
+
+    console.log('------------------------------');
+
+    if (event) {
+        console.log(event.artist.name);
+        console.log(`Venue: ${event.venue.name}`);
+        console.log(`Location: ${event.venue.city}, ${event.venue.region}`);
+
+        let date = moment(event.datetime).format('MM/DD/YYYY');
+        console.log(`Date: ${date}`);
+    } else {
+        console.log("No Results");
+    }
+
+    console.log('------------------------------');
+    });
+
+}
+
+function SpotifyThisSong() {
+
+}
+
+function MovieThis() {
+
+}
+
+function DoWhatItSays() {
+
+}
