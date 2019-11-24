89431b7c11713b3b4fa3c47728b16c84780c8c76
ajreid <axr490@miami.edu>
2019-11-23 20:12:06 -0500

    added functionality for 'spotify-this-song' action

--- liri.js
+++ liri.js
@@ -11,14 +11,12 @@ let spotify = new Spotify(keys.spotify);
 let action = process.argv[2];
 let search = process.argv.slice(3);
 
-console.log(action);
-console.log(search);
-
 switch (action) {
     case "concert-this":
         ConcertThis(search.join("+"));
         break;
     case "spotify-this-song":
+        SpotifyThisSong(search.join(" "));
         break;
     case "movie-this":
         break;
@@ -34,15 +32,15 @@ function ConcertThis(q) {
 
     console.log('------------------------------');
 
-    if (event) {
+    if (!event) {
+        console.log("No Results");
+    } else {
         console.log(event.artist.name);
         console.log(`Venue: ${event.venue.name}`);
         console.log(`Location: ${event.venue.city}, ${event.venue.region}`);
 
         let date = moment(event.datetime).format('MM/DD/YYYY');
         console.log(`Date: ${date}`);
-    } else {
-        console.log("No Results");
     }
 
     console.log('------------------------------');
@@ -50,7 +48,30 @@ function ConcertThis(q) {
 
 }
 
-function SpotifyThisSong() {
+function SpotifyThisSong(q) {
+
+    spotify.search({type: 'track', query: q}, function(err, data) {
+        if (err) {
+            return console.log("Error Occured: " + err);
+        }
+
+        let song = data.tracks.items[0];
+
+        console.log('------------------------------');
+
+        if (!song) {
+            console.log("No Results");
+        } else {
+
+            console.log(`Artist: ${song.artists[0].name}`);
+            console.log(`'${song.name}'`);
+            console.log(`Preview: ${song.external_urls.spotify}`);
+            console.log(`Album: ${song.album.name}`);
+        }
+
+        console.log('------------------------------');
+
+    });
 
 }
 
